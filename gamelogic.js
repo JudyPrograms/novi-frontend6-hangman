e98e301a64e7e7fa4a7f14771c738d88911b335e
JudyPrograms <judithkats@gmail.com>
2021-07-19 12:59:04 +0200

    hangman completed

--- gamelogic.js
+++ gamelogic.js
@@ -1,13 +1,50 @@
 function displayWordSoFar(word, guesses) {
-  // WRITE ME
+
+  // creëer display
+  const display = []
+  for (let i = 0; i < word.length; i++) {
+    display.push("_")
+  }
+  // als de guess in het woord voorkomt
+  // dan check op welke indices
+  // en plaats de guess in de display op alle indices
+
+  for (let i = 0; i < guesses.length; i++) {
+    if (word.includes(guesses[i])) {
+      const indices = []
+      for (let j = 0; j < word.length; j++) {
+        if (word[j] === guesses[i]) {
+          indices.push(j)
+        }
+      }
+      for (let j = 0; j < indices.length; j++) {
+        display.splice(indices[j], 1, guesses[i])
+      }
+    }
+  }
+  return display.join(" ") + " "
 }
 
 function isGameWon(word, guesses) {
-  // WRITE ME
+  // als niet alle letters van word in guess
+  // dan false
+  for (let i = 0; i < word.length; i++) {
+    if (!guesses.includes(word[i])) {
+      return false
+    } else {
+      return true
+    }
+  }
 }
 
 function isGameLost(word, guesses) {
-  // WRITE ME
+  let wrongCount = 0
+  for (let i = 0; i < guesses.length; i++) {
+    if (!word.includes(guesses[i])) {
+      wrongCount ++
+    }
+  }
+  return wrongCount >= 7;
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+// Zorg dat de gebruiker letters kan raden
+// Houdt in de guesses-array bij welke letters de gebruiker geraden heeft
+// Laat zien welke letters al geraden zijn (met displayWordSoFar)
+// Heeft iemand alle letters geraden? Wuhuuu! Log dan dat ze gewonnen hebben
+// Heeft iemand 7 letters fout geraden? Jammer! Log dan dat ze verloren hebben
+// Stop het programma met return wanneer iemand gewonnen of verloren heeft
+// Heeft iemand nog niet gewonnen of verloren? Dan gaat het spel gewoon verder
+
+
 const { question } = require("readline-sync");
 const { displayWordSoFar, isGameWon, isGameLost } = require("./gamelogic");
 
